f3d1700b659feaa55a66bc16c5fa772742cf23bd
104822 <69157071+104822@users.noreply.github.com>
2020-10-09 19:16:23 +0530

    Add files via upload

---
+++ implementation/inc/main.h
@@ -0,0 +1,55 @@
+/**
+* @file main.h
+*
+*/
+#include <stdio.h>
+
+/**
+*Generates the display options
+*/
+
+#ifndef __diplay_options_H__
+#define __display_options_H__
+void display_options();
+#endif // __diplay_options_H__
+
+/**
+*Helps to create new account
+*/
+
+#ifndef __Create_new_account_H__
+#define __Create_new_account_H__
+void Create_new_account();
+#endif // __Create_new_account_H__
+
+/**
+*provides all the account information
+*/
+
+#ifndef __Account_information_H__
+#define __Account_information_H__
+void Account_information();
+#endif // __Account_information_H__
+
+/**
+*Provides the information of the cash deposited
+*/
+
+#ifndef __Cash_deposit_H__
+#define __Cash_deposit_H__
+void Cash_Deposit();
+#endif // __Cash_deposit_H__
+
+/**
+*Provides the information of the cash withdrawl
+*/
+
+#ifndef __Cash_withdrawal_H__
+#define __Cash_withdrawal_H__
+void Cash_withdrawl();
+#endif // __Cash_withdrawal_H__
+
+
+
+
+
